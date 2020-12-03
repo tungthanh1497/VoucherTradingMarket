ccbe852a538cad5f8a6432fce3aca3c538d1487c
tungtt <tungthanh1497@gmail.com>
2020-12-03 10:34:21 +0700

    Initial commit

---
+++ app/src/main/java/com/tungtt/vtm/ui/login/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.tungtt.vtm.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tungtt.vtm.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/tungtt/vtm/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.tungtt.vtm.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
